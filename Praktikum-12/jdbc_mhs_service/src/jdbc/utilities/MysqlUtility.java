f1706cf78836c01b134ce0ee6812f09278bb9804
xazhurea <160566357+xazhurea@users.noreply.github.com>
2024-06-05 17:48:52 +0700

    Add files via upload

---
+++ Praktikum-12/jdbc_mhs_service/src/jdbc/utilities/MysqlUtility.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jdbc.utilities;
+import java.sql.*;
+
+/**
+ *
+ * @author Fathia Rahma
+ */
+public class MysqlUtility {
+    private static Connection koneksi;
+    
+    public static Connection getConnection() {
+        if (koneksi == null) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                String url = "jdbc:mysql://localhost:3306/pbo";
+                String user = "root";
+                String password = "";
+                koneksi = DriverManager.getConnection(url, user, password);
+                
+            } catch (ClassNotFoundException cne){
+                System.out.println("Gagal load driver : " + cne.getMessage());
+            } catch (SQLException sqle) {
+                System.out.println("Gagal Koneksi : " + sqle.getMessage());
+            }
+        }
+        
+        return koneksi;
+    }
+}
